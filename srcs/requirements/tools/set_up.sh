39671a8cd7ce7d2c7d069dac7cf26f27f2a18345
chanyeop <153893@naver.com>
2022-07-31 21:17:32 +0900

    Feat: set mariadb, nginx, wordpress

---
+++ srcs/requirements/tools/set_up.sh
@@ -0,0 +1,7 @@
+apt-get install docker -y
+apt-get install docker.compose -y
+apt-get install make -y
+mkdir /home/clim/data
+mkdir /home/clim/data/wordpress
+mkdir /home/clim/data/mariadb
+cp ./srcs/requirements/tools/hosts /etc/hosts
